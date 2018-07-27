f84aa2904cff517617d1a5366310ed4d5738321f
Maciej Krüger <mkg20001@gmail.com>
2018-07-27 15:48:25 +0200

    docs: Add example

--- src/forwardAddr/parser/index.js
+++ src/forwardAddr/parser/index.js
@@ -4,6 +4,22 @@ const tokenizer = require('./tokenizer')
 const astifier = require('./astifier')
 const compiler = require('./compiler')
 
+/*
+
+Example:
+
+/tcp/.port/5323/ssl/.hostname/*.example.com/http/.path/"/myservice"/_ws/stream/
+
+[
+  {protocol: 'tcp', conditions: {port: {compare: 'strict', value: 5323}, action: 'stream'},
+  {protocol: 'ssl', conditions: {hostname: {compare: 'glob', value: '*.example.com'}}, action: 'stream'},
+  {protocol: 'http', conditions: {path: {compare: 'strict', value: '/myservice'}}, action: 'sub', sub: {
+    protocol: 'ws', conditions: {}, action: 'stream'
+  }},
+]
+
+*/
+
 module.exports = function parser (str) {
   return compiler(astifier(tokenizer(str)))
 }
